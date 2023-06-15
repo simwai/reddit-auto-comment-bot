d4ae7de8b0f85799d6833e632e8a5b7b7d523e7f
Simon Waiblinger <SimonWaiblinger@live.at>
2023-06-15 17:09:32 +0200

    Updated deploy script for Ubuntu server

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create and activate a virtual environment (optional but recommended)
-python3 -m venv myenv
+python3.10 -m venv myenv
 source myenv/bin/activate
 
 # Install dependencies
-pip install -r requirements.txt
+python3.10 -m pip install praw
 
 # Run main.py or index.py depending on what exists
 if [ -f main.py ]; then
-    python3 main.py
+    python3.10 main.py
 elif [ -f index.py ]; then
-    python3 index.py
+    python3.10 index.py
 fi
