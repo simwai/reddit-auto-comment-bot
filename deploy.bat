47263cfb3c4f395296d63abcf0135b80f055a263
Simon Waiblinger <SimonWaiblinger@live.at>
2023-07-02 22:17:01 +0200

    Updated python version in the deploy scripts to 3.6

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: Create and activate a virtual environment (optional but recommended)
-python3 -m venv myenv
+python3.6 -m venv myenv
 call myenv/Scripts/activate.bat
 
 :: Install dependencies
@@ -9,7 +9,7 @@ pip3 install -r requirements.txt
 
 :: Run main.py or index.py depending on what exists
 if exist main.py (
-    python3 main.py
+    python3.6 main.py
 ) else if exist index.py (
-    python3 index.py
+    python3.6 index.py
 )
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Create and activate a virtual environment (optional but recommended)
-python3.10 -m venv myenv
+python3.6 -m venv myenv
 source myenv/bin/activate
 
 # Install dependencies
-python3.10 -m pip install praw
+python3.6 -m pip install praw
 
 # Run main.py or index.py depending on what exists
 if [ -f main.py ]; then
-    python3.10 main.py
+    python3.6 main.py
 elif [ -f index.py ]; then
-    python3.10 index.py
+    python3.6 index.py
 fi
